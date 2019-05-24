cd86e2978f8c582fd44cd5dd6c09f22f86fde6da
DovgaNik <nikita.dovgan.05@hotmail.com>
2019-05-25 00:22:09 +0300

    Added some changes at readme file

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ Markdown viewer
 Dependencies:
 ---
 
-* markdown
+* markdown(you can install markdown from your package manager)
+	* pacman -S markdown #for arch linux
+	* apt-get install markdown #for debian-based linux distrubutions
--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,18 @@
 
 using namespace std;
 
-string command;
+string command,
+       md_filename,
+       html_filename;
 int l;
 
 int main(int argc, char *argv[]){
+	md_filename = string(argv[1]);
+	html_filename = string(argv[2]);
+//Debug code
+//	cout << "Markdown filename is " << md_filename << endl;
+//	cout << "Output html filename is " << html_filename << endl;
+
 	command = "markdown -html5 " + string(argv[1]) + " >> " + string(argv[2]);
 	l = command.length();
 	char command_char[l];
