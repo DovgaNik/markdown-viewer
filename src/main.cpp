973d6fe01e0f924a9f68c869e71441990ab8971d
DovgaNik <nikita.dovgan.05@hotmail.com>
2019-05-25 21:21:13 +0300

    Added coments on code

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include<iostream>
+#include<fstream>
 
 using namespace std;
 
 string command,
        md_filename,
        html_filename;
-int l;
+int l,
+    update_time;
 
-void msleep(unsigned long milisec)
-{
+void msleep(unsigned long milisec){
     struct timespec req={0};
     time_t sec=(int)(milisec/1000);
     milisec=milisec-(sec*1000);
@@ -19,23 +20,23 @@ void msleep(unsigned long milisec)
 }
 
 int main(int argc, char *argv[]){
-	md_filename = string(argv[1]);
-	html_filename = string(argv[2]);
-//Debug code
-//	cout << "Markdown filename is " << md_filename << endl;
-//	cout << "Output html filename is " << html_filename << endl;
+	md_filename = string(argv[1]); //Markdown filename
+	html_filename = string(argv[2]); //Output (html5) filename
+	update_time = atof(argv[3]) * 1000; //Time between output file updates
+	command = "markdown -html5 " + md_filename + " >> " + html_filename; //Command that will compile markdown code to html5 code
+	l = command.length(); //Length of the command
+	char command_char[l]; //Command but in char array
 
-	command = "markdown -html5 " + string(argv[1]) + " >> " + string(argv[2]);
-	l = command.length();
-	char command_char[l];
-	for(int i = 0; i < l; i++){
+	//Converting string to char array	
+	for(int i = 0; i < l; i++)
 		command_char[i] = command[i];
-	}
-//	for(int i = 0; i < l; i++){
-//		cout << command_char[i];	
-//	}
+
+	ofstream clear; //Creating ofstream object for clearing the file
 	while(true){
-		system(command_char);
-		msleep(10000);
+		clear.open(html_filename); //Opening output (html5) file
+		clear.close(); //Closing output (html5) file for clearing output file
+		
+		system(command_char); //Performing command
+		msleep(update_time); //Delay between updates
 	}
 }
